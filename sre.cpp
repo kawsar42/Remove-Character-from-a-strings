28b086d38a88c67b7cada9b090096b810f5d0603
kawsar42 <59926691+kawsar42@users.noreply.github.com>
2022-09-04 18:05:00 +0600

    Add files via upload

---
+++ sre.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+void solve()
+{
+
+}
+int main()
+{
+    int t;
+
+    string s;
+    char c;
+    cin >> c;
+    getline(cin,s);
+    s=c+s;
+    int a,b;
+    cin >> a>> b;
+    s.erase(a,b);
+    cout << s << endl;
+
+   // cout << s.at(4) << endl;
+   // s.clear();
+    //cout << s << endl;
+}
